8028a629cc1d9f004f2dda8ed10e4da5a6683a88
ismailszr <127437104+ismailszr@users.noreply.github.com>
2023-03-15 13:21:27 +0300

    Update Mentoring02Class.java

--- src/main/java/mentoring01/Mentoring02Class.java
+++ src/main/java/mentoring01/Mentoring02Class.java
@@ -2,6 +2,6 @@ package mentoring01;
 
 public class Mentoring02Class {
     public static void main(String[] args) {
-        System.out.println("mentoring02 class");
+        System.out.println("mentoring02 class ismail");
     }
 }
